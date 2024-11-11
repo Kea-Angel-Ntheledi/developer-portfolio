388be6ac2d18e0ef7510f70a8c80bf469014e185
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-11-11 09:55:13 +0200

    Updated aboutData.js: changed description2 text

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "Hello! My name is Kealeboga Angel Ntheledi. I'm an aspiring Junior Software Developer residing in Johannesburg and Rustenburg.",
-    description2: "I am currently a student at CodeSpace Academy, learning full stack development. As part of my studies, I'm working on a capstone project that’s close to my heart: building a full stack e-commerce website from the ground up. This project allows me to apply everything I've learned in both front-end and back-end development, from designing user-friendly interfaces to creating secure, efficient server-side logic. I'm excited to continue honing my skills and bringing this project to life as the year comes to a close. My goal is to create an engaging, scalable platform that combines functionality with a seamless user experience. Outside of coding, I am a bibliophile who loves not only reading books but also curating a personal library.",
+    description2: "As a software engineer, I specialize in building fully responsive, high-quality digital projects tailored to meet user needs. My expertise includes developing dynamic e-commerce stores, engaging webpages, versatile websites, and efficient applications. I take pride in crafting seamless user experiences across all devices, ensuring that each project is fully optimized for responsiveness, functionality, and aesthetic appeal. From concept to deployment, I am dedicated to creating impactful digital solutions that drive results and enhance user interaction.",
     image: 2
 }
